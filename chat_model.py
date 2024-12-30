fd27d9531abdfbb8967f7f81b882bf534b25b610
nickoboco <nickolas.selhorst@gmail.com>
2024-12-30 14:54:03 -0300

    adicionado comentários no código

--- chat_model.py
+++ chat_model.py
@@ -1,11 +1,15 @@
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 
+# Carrega as variáveis de ambiente
 load_dotenv()
 
+# Instancia o modelo de chat
 model = ChatOpenAI(model='gpt-4o-mini')
 
+# Faz uma pergunta ao modelo
 result = model.invoke('Quanto é 2 + 2?')
 
-print("Full result:", result)
-print("Content only:", result.content)
+# Exibe os resultados
+print("Exemplo de retorno completo: ", result)
+print("Exemplo de retorno somente com a resposta: ", result.content)
